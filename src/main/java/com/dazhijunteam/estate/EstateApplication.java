2eb2cdcf3f3ac0417ee2825f232abbb09384d188
dazhijun <1427335446@qq.com>
2018-03-19 14:58:34 +0800

    init

---
+++ src/main/java/com/dazhijunteam/estate/EstateApplication.java
@@ -0,0 +1,12 @@
+package com.dazhijunteam.estate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstateApplication.class, args);
+	}
+}
